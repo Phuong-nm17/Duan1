14b88cd5eeb18538da61c3fd0f80e524346346b1
Huongxinh2912 <158167431+Huongxinh2912@users.noreply.github.com>
2025-03-26 08:31:33 +0700

    Huong (#9)
    * huong
    
    * cc
    
    * home
    
    * tìm kiếm
    
    ---------
    
    Co-authored-by: Huong <nthuong.su20@gmail.com>

--- view/ProductList.php
+++ view/ProductList.php
@@ -5,9 +5,16 @@ require_once(__DIR__ . '/../model/connect.php');
 
 try {
 
-    $sql = "SELECT * FROM product;";
+    $search = isset($_GET['search']) ? trim($_GET['search']) : '';
 
-    $stmt = $conn->prepare($sql);
+    if (!empty($search)) {
+        $sql = "SELECT * FROM product WHERE title LIKE :search";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindValue(':search', "%$search%", PDO::PARAM_STR);
+    } else {
+        $sql = "SELECT * FROM product";
+        $stmt = $conn->prepare($sql);
+    }
 
     $stmt->execute();
 
@@ -44,7 +51,8 @@ try {
 
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
 
     <!-- Font Awesome -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
@@ -92,17 +100,20 @@ try {
         <div class="row align-items-center py-3 px-xl-5">
             <div class="col-lg-3 d-none d-lg-block">
                 <a href="" class="text-decoration-none">
-                    <h1 class="m-0 display-5 font-weight-semi-bold"><span class="text-primary font-weight-bold border px-3 mr-1">E</span>Shopper</h1>
+                    <h1 class="m-0 display-5 font-weight-semi-bold"><span
+                            class="text-primary font-weight-bold border px-3 mr-1">E</span>Shopper</h1>
                 </a>
             </div>
             <div class="col-lg-6 col-6 text-left">
-                <form action="">
+                <form action="index.php" method="GET">
+                    <input type="hidden" name="act" value="ProductList">
                     <div class="input-group">
-                        <input type="text" class="form-control" placeholder="Search for products">
+                        <input name="search" type="text" class="form-control" placeholder="Search for products"
+                            value="<?= htmlspecialchars($_GET['search'] ?? '') ?>">
                         <div class="input-group-append">
-                            <span class="input-group-text bg-transparent text-primary">
+                            <button type="submit" class="input-group-text bg-transparent text-primary">
                                 <i class="fa fa-search"></i>
-                            </span>
+                            </button>
                         </div>
                     </div>
                 </form>
@@ -125,22 +136,26 @@ try {
 <div class="container-fluid">
     <div class="row border-top px-xl-5">
         <div class="col-lg-3 d-none d-lg-block">
-                <a class="btn shadow-none d-flex align-items-center justify-content-between bg-primary text-white w-100" data-toggle="collapse" href="#navbar-vertical" style="height: 65px; margin-top: -1px; padding: 0 30px;">
+            <a class="btn shadow-none d-flex align-items-center justify-content-between bg-primary text-white w-100"
+                data-toggle="collapse" href="#navbar-vertical" style="height: 65px; margin-top: -1px; padding: 0 30px;">
                 <h6 class="m-0">Categories</h6>
                 <i class="fa fa-angle-down text-dark"></i>
             </a>
-            <nav class="collapse position-absolute navbar navbar-vertical navbar-light align-items-start p-0 border border-top-0 border-bottom-0 bg-light" id="navbar-vertical" style="width: calc(100% - 30px); z-index: 1;">
+            <nav class="collapse position-absolute navbar navbar-vertical navbar-light align-items-start p-0 border border-top-0 border-bottom-0 bg-light"
+                id="navbar-vertical" style="width: calc(100% - 30px); z-index: 1;">
                 <div class="navbar-nav w-100 overflow-hidden" style="height: 120px">
-                <?php foreach ($category as $cat) : ?>
-                    <a href="index.php?act=cate&id=<?= $cat['id'] ?>" class="nav-item nav-link"><?= htmlspecialchars($cat['name']) ?></a>
-                <?php endforeach; ?>
-                    </div>
+                    <?php foreach ($category as $cat) : ?>
+                        <a href="index.php?act=cate&id=<?= $cat['id'] ?>"
+                            class="nav-item nav-link"><?= htmlspecialchars($cat['name']) ?></a>
+                    <?php endforeach; ?>
+                </div>
             </nav>
         </div>
         <div class="col-lg-9">
             <nav class="navbar navbar-expand-lg bg-light navbar-light py-3 py-lg-0 px-0">
                 <a href="" class="text-decoration-none d-block d-lg-none">
-                    <h1 class="m-0 display-5 font-weight-semi-bold"><span class="text-primary font-weight-bold border px-3 mr-1">E</span>Shopper</h1>
+                    <h1 class="m-0 display-5 font-weight-semi-bold"><span
+                            class="text-primary font-weight-bold border px-3 mr-1">E</span>Shopper</h1>
                 </a>
                 <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                     <span class="navbar-toggler-icon"></span>
@@ -181,37 +196,77 @@ try {
     </div>
 </div>
 <!-- Page Header End -->
-<!-- Products start -->
-<div class="container-fluid pt-5">
-    <div class="text-center mb-4">
-        <h2 class="section-title px-5"><span class="px-2">Shop</span></h2>
-    </div>
-    <div class="row px-xl-5 pb-3">
-        <?php foreach ($product as $p) : ?>
-            <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
-                <div class="card product-item border-0 mb-4">
-                    <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
-                        <img class="img-fluid w-100" src="<?= $p['thumbnail'] ?>" alt="">
-                    </div>
-                    <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
-                        <h6 class="text-truncate mb-3"><?= $p['title'] ?></h6>
-                        <div class="d-flex justify-content-center">
-                            <h6><?= number_format($p['discount']) ?> đ</h6>
-                            <h6 class="text-muted ml-2"><del><?= number_format($p['price']) ?> đ</del></h6>
+<?php
+if (isset($_GET['search'])): ?>
+    <h2 class="text-primary text-uppercase mb-3" style="margin-left: 40px;">
+        Kết quả tìm kiếm cho: "<?= htmlspecialchars($_GET['search']) ?>"
+    </h2>
+
+
+    <?php if (empty($product)): ?>
+        <p class="text-danger mb-3" style="margin-left: 60px; font-size: 20px; font-weight: bold;">Không tìm thấy sản phẩm nào.
+        </p>
+    <?php else: ?>
+        <div class=" row pb-3 px-xl-5">
+            <?php foreach ($product as $p) : ?>
+                <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
+                    <div class="card border-0 mb-4 product-item">
+                        <div class="card-header bg-transparent border p-0 position-relative overflow-hidden product-img">
+                            <img class="w-100 img-fluid" src="<?= $p['thumbnail'] ?>" alt="">
+                        </div>
+                        <div class="card-body border-left border-right p-0 text-center pb-3 pt-4">
+                            <h6 class="text-truncate mb-3"><?= $p['title'] ?></h6>
+                            <div class="d-flex justify-content-center">
+                                <h6> $ <?= number_format($p['discount']) ?></h6>
+                                <h6 class="text-muted ml-2"><del>$<?= number_format($p['price']) ?></del></h6>
+                            </div>
+                        </div>
+                        <div class="d-flex card-footer bg-light border justify-content-between">
+                            <a href="index.php?act=ProductDetail&id=<?= $p['id'] ?>" class="btn btn-sm p-0 text-dark"><i
+                                    class="text-primary fa-eye fas mr-1"></i>View Detail</a>
+                            <a href="" class="btn btn-sm p-0 text-dark"><i class="text-primary fa-shopping-cart fas mr-1"></i>Add To
+                                Cart</a>
                         </div>
                     </div>
-                    <div class="card-footer d-flex justify-content-between bg-light border">
-                        <a href="index.php?act=ProductDetail&id=<?= $p['id'] ?>" class="btn btn-sm text-dark p-0"><i class="fas fa-eye text-primary mr-1"></i>View Detail</a>
-                        <a href="index.php?act=ProductDetail&id=<?= $p['id'] ?>" class="btn btn-sm text-dark p-0"><i class="fas fa-shopping-cart text-primary mr-1"></i>Add To Cart</a>
+                </div>
+            <?php endforeach; ?>
+        </div>
+        </div>
+    <?php endif; ?>
+<?php else: ?>
+    <!-- Products start -->
+    <div class="container-fluid pt-5">
+        <div class="text-center mb-4">
+            <h2 class="section-title px-5"><span class="px-2">Shop</span></h2>
+        </div>
+        <div class="row px-xl-5 pb-3">
+            <?php foreach ($product as $p) : ?>
+                <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
+                    <div class="card product-item border-0 mb-4">
+                        <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
+                            <img class="img-fluid w-100" src="<?= $p['thumbnail'] ?>" alt="">
+                        </div>
+                        <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
+                            <h6 class="text-truncate mb-3"><?= $p['title'] ?></h6>
+                            <div class="d-flex justify-content-center">
+                                <h6><?= number_format($p['discount']) ?> đ</h6>
+                                <h6 class="text-muted ml-2"><del><?= number_format($p['price']) ?> đ</del></h6>
+                            </div>
+                        </div>
+                        <div class="card-footer d-flex justify-content-between bg-light border">
+                            <a href="index.php?act=ProductDetail&id=<?= $p['id'] ?>" class="btn btn-sm text-dark p-0"><i class="fas fa-eye text-primary mr-1"></i>View Detail</a>
+                            <a href="" class="btn btn-sm text-dark p-0"><i class="fas fa-shopping-cart text-primary mr-1"></i>Add To Cart</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-        <?php endforeach; ?>
+            <?php endforeach; ?>
+        </div>
     </div>
-</div>
+<?php endif; ?>
+
 <!-- Products End -->
-      <!-- Back to Top -->
-      <a href="#" class="btn btn-primary back-to-top"><i class="fa fa-angle-double-up"></i></a>
+<!-- Back to Top -->
+<a href="#" class="btn btn-primary back-to-top"><i class="fa fa-angle-double-up"></i></a>
 
 
 <!-- JavaScript Libraries -->
