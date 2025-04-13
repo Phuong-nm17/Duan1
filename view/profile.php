23c0070aaf1188b16f289aa7879d6777a191bba4
linhnq9904 <linhnqph46354@fpt.edu.vn>
2025-04-13 12:50:51 +0700

    update

--- view/profile.php
+++ view/profile.php
@@ -261,7 +261,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             </form>
         </div>
     </div>
-
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
     <script src="view/lib/easing/easing.min.js"></script>
